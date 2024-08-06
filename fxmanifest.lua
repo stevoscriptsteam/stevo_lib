ae88ea4d33bd6cd013b846d19e7f61679c0e2bb9
steve <stevothedev@gmail.com>
2024-08-06 12:07:16 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,8 @@ use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 
 author "Stevo Scripts | steve"
-description 'A library of functions & a bridge for Stevo Scripts resources.'
-version '1.1.0'
+description 'A library of functions & bridge for Stevo Scripts resources.'
+version '1.1.5'
 
 shared_script {
     '@ox_lib/init.lua',
